0ec40312d4d22163cd7ffc3c11e34e4cc1d3c079
Sören Berger <soeren.berger@u1337.de>
2017-03-11 18:01:32 +0100

    Added puppet-lint to vagrant box

--- vagrant/init.sh
+++ vagrant/init.sh
@@ -6,3 +6,9 @@ mkdir -p /etc/puppet/modules
 mkdir -p /etc/puppet/hiera
 test -e /etc/puppet/modules/concat || puppet module install puppetlabs-concat
 test -e /etc/puppet/modules/stdlib || puppet module install puppetlabs-stdlib
+
+# Install puppet-lint
+gem install puppet-lint
+# usage in the vagrant box:
+# $ puppet-lint /vagrant
+
