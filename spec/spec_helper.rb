44fe5f23ae3572389a6d195a23d001086cf667ee
Soeren Berger <soeren.berger@tik.uni-stuttgart.de>
2017-03-12 18:54:10 +0100

    Added basic rspec tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,14 @@
-require 'puppetlabs_spec_helper/module_spec_helper'
+require 'rspec-puppet'
+
+fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
+
+RSpec.configure do |c|
+  c.module_path = '/etc/puppet/modules'
+  c.manifest_dir = File.join(fixture_path, 'manifests')
+
+  # Coverage generation
+  c.after(:suite) do
+    RSpec::Puppet::Coverage.report!
+  end
+
+end
