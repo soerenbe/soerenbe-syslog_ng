f722efda25394aa1a27f700022a0a4c53d9ce8ea
pythophanatic <max.schumacher96@icloud.com>
2019-01-08 16:23:37 +0100

    append new tls_settings to destination specs

--- manifests/destination/network.pp
+++ manifests/destination/network.pp
@@ -14,25 +14,25 @@ define syslog_ng::destination::network (
   case $proto {
     'UDP', 'udp': {
       syslog_ng::destination { $name:
-        spec   => "udp('${log_server}' port(${log_port}) log_fifo_size(${::syslog_ng::log_fifo_size_destination}));",
+        spec   => "udp('${log_server}' port(${log_port}) log_fifo_size(${::syslog_ng::log_fifo_size_destination}) ${tls_settings});",
         target => $syslog_ng::params::config_file_destination_remote
       }
     }
     'UDP6', 'udp6': {
       syslog_ng::destination { $name:
-        spec   => "udp6('${log_server}' port(${log_port}) log_fifo_size(${::syslog_ng::log_fifo_size_destination}));",
+        spec   => "udp6('${log_server}' port(${log_port}) log_fifo_size(${::syslog_ng::log_fifo_size_destination}) ${tls_settings});",
         target => $syslog_ng::params::config_file_destination_remote
       }
     }
     'TCP', 'tcp': {
       syslog_ng::destination { $name:
-        spec   => "tcp('${log_server}' port(${log_port}) log_fifo_size(${::syslog_ng::log_fifo_size_destination}));",
+        spec   => "tcp('${log_server}' port(${log_port}) log_fifo_size(${::syslog_ng::log_fifo_size_destination}) ${tls_settings});",
         target => $syslog_ng::params::config_file_destination_remote
       }
     }
     'TCP6', 'tcp6': {
       syslog_ng::destination { $name:
-        spec   => "tcp6('${log_server}' port(${log_port}) log_fifo_size(${::syslog_ng::log_fifo_size_destination}));",
+        spec   => "tcp6('${log_server}' port(${log_port}) log_fifo_size(${::syslog_ng::log_fifo_size_destination}) ${tls_settings});",
         target => $syslog_ng::params::config_file_destination_remote
       }
     }
